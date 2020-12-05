ccfa937cf5e29f153875da5a615949d40a9df9c4
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-12-05 07:22:56 -0800

    Update numberOfCommonStrings.py

--- numberOfCommonStrings.py
+++ numberOfCommonStrings.py
@@ -1,17 +1,10 @@
 def commonChars(s1, s2):
-    s = None
-    if (len(s1) < len(s2)):
-        s = s1
-        l = s2
-    else:
-        s = s2
-        l = s1
-
     c = 0
-    for i in s:
-        if i in l:
+    cT = []
+    for i in s1:
+        if i in s2 and i not in cT:
             c = c + 1
-    return c
-    
+            cT.append(i)
+    return c  
 
-print(commonChars('hello','world'))
+print(commonChars('hellt','worrdl'))
