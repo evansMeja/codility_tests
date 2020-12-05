c4148117e11bc6f3448c9052ddcb6c98f801674b
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-12-05 07:54:41 -0800

    Update numberOfCommonStrings.py

--- numberOfCommonStrings.py
+++ numberOfCommonStrings.py
@@ -5,6 +5,34 @@ def commonChars(s1, s2):
         if i in s2 and i not in cT:
             c = c + 1
             cT.append(i)
-    return c  
+    t = 0
 
-print(commonChars('hellt','worrdl'))
+    for i in cT:
+        oc = 0
+        time_it_appears_s1 = 0
+        for x in s1:
+            if i == x:
+                time_it_appears_s1 = time_it_appears_s1 + 1
+        
+        time_it_appears_s2 = 0
+
+        print(time_it_appears_s1)
+        
+        for x in s2:
+            if i == x:
+                time_it_appears_s2 = time_it_appears_s2 + 1
+
+        print(time_it_appears_s2)
+
+        if time_it_appears_s1 < time_it_appears_s2:
+            oc = time_it_appears_s1
+        else:
+            if time_it_appears_s2 < time_it_appears_s1:
+                oc = time_it_appears_s2
+            else:
+                if time_it_appears_s2 == time_it_appears_s2:
+                    oc = time_it_appears_s2
+        t = t + oc
+    return t
+
+print(commonChars('helltth','worrdllh'))
