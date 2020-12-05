95f7d912c17cf5e4fa61ede8e8913984f58f2042
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-12-05 08:24:21 -0800

    optimized the algorithm

--- numberOfCommonStrings.py
+++ numberOfCommonStrings.py
@@ -8,38 +8,21 @@ example
 def commonChars(s1, s2):
     c = 0
     cT = []
+    t=0
+    c = []
     for i in s1:
-        if i in s2 and i not in cT:
-            c = c + 1
-            cT.append(i)
-    t = 0
-
-    for i in cT:
-        oc = 0
-        time_it_appears_s1 = 0
-        for x in s1:
-            if i == x:
-                time_it_appears_s1 = time_it_appears_s1 + 1
-        
-        time_it_appears_s2 = 0
-
-        print(time_it_appears_s1)
-        
-        for x in s2:
-            if i == x:
-                time_it_appears_s2 = time_it_appears_s2 + 1
-
-        print(time_it_appears_s2)
-
-        if time_it_appears_s1 < time_it_appears_s2:
-            oc = time_it_appears_s1
-        else:
-            if time_it_appears_s2 < time_it_appears_s1:
-                oc = time_it_appears_s2
-            else:
-                if time_it_appears_s2 == time_it_appears_s2:
-                    oc = time_it_appears_s2
-        t = t + oc
+        if i not in c:
+            oc = 0
+            time_it_appears_s1 = s1.count(i)
+            time_it_appears_s2 = s2.count(i)
+            if time_it_appears_s1 != 0 and time_it_appears_s2 != 0:   
+                if time_it_appears_s1 < time_it_appears_s2:
+                    oc = time_it_appears_s1
+                else:
+                    if time_it_appears_s2 < time_it_appears_s1:
+                        oc = time_it_appears_s2
+                    else:
+                        oc = time_it_appears_s2
+                t = t + oc
+            c.append(i)
     return t
-
-print(commonChars('helltth','worrdllh'))
